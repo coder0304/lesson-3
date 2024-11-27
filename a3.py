c56d4343299b7f4a23dd8836b5b5f4b3a4117c5f
coder0304 <nellikatgowri@gmail.com>
2024-11-27 19:19:22 +0530

    a3.py

---
+++ a3.py
@@ -0,0 +1,11 @@
+x="GowRiNandA NElliKat"
+print(str.lower(x))
+print(str.upper(x))
+
+print("Enter the first string:")
+strOne= input()
+print("Enter the second string:")
+strTwo= input()
+
+strThree= strOne +" "+ strTwo
+print("\n Concatenated String:",strThree)
